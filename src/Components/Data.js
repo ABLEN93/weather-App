97064a6dae3ba86f1a584fd5573fa335c413f720
ABLEN93 <iamme5643@gmail.com>
2021-08-25 09:27:41 +0200

    css header

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,33 +1,29 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 function Data() {
-
     const [info, setInfo] = useState({
         city: "",
-        country:""
+        country: ""
     });
     const [storage, setStorage] = useState([]);
-
     const Url = `https://api.openweathermap.org/data/2.5/weather?q=${info.city},${info.country}&appid=9442fd74cbb33752ba35d0918a6c892b`;
-
     function getWheather(e) {
         e.preventDefault();
-        if(info.city!==""&&info.country!==""){fetch(Url)
-            .then((result) => result.json())
-            .then((data)=>setStorage([data],console.log("working",storage)))
+        if (info.city !== "" && info.country !== "") {
+            fetch(Url)
+                .then((result) => result.json())
+                .then((data) => setStorage([data], console.log("working", storage)))
         } else {
             alert("fill out this one")
         }
-        
     }
     return (
         <div class="main-box">
-            <form onSubmit={(e)=>getWheather(e)}>
-                <input onChange={(e)=> setInfo({...info,city:e.target.value})} type="text" placeholder="city" />
-                <input onChange={(e)=> setInfo({...info,country:e.target.value})} type="text" placeholder="country"/>
+            <form onSubmit={(e) => getWheather(e)}>
+                <input onChange={(e) => setInfo({ ...info, city: e.target.value })} type="text" placeholder="city" />
+                <input onChange={(e) => setInfo({ ...info, country: e.target.value })} type="text" placeholder="country" />
                 <button>get Wheather</button>
             </form>
-
             <div class="info">
                 {storage.map((data, index) => <div key={index + 1}>
                     <h1 class="card-body">City name: {data.name}</h1>
@@ -36,19 +32,10 @@ function Data() {
                     <p class="card-body">Temperature: {data.main.temp}</p>
                     <p class="card-body">Timezone: {data.timezone}</p>
                     <p class="card-body">Visibility: {data.visibility}</p>
-
-            <div>
-                {storage.map((data, index) => <div key={index +1}>
-                    <h1>City: {data.name}</h1>
-                    <p>Weather: {data.weather[0].main}</p>
-                    <p>Timezone: {data.timezone}</p>
-                    <p>Temperature: {data.main.temp} fahrenheit</p>
-
-
-                </div>)}
+                </div>
+                )}
+                </div>
             </div>
-        </div>
     )
 }
-
-export default Data
+export default Data;
